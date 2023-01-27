37afbcb2f5955dae5a9bd57955d27120643a036f
J D <zathrath03@gmail.com>
2023-01-26 20:32:08 -0800

    Unit tests for the three cases of Reservation.CanBeCancelledBy

--- source-code-starter/source-code-starter/TestNinja/TestNinja.UnitTests/ReservationsTests.cs
+++ source-code-starter/source-code-starter/TestNinja/TestNinja.UnitTests/ReservationsTests.cs
@@ -11,8 +11,22 @@ namespace TestNinja.UnitTests
         public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
         {
             var reservation = new Reservation();
-            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
-            Assert.IsTrue(result);
+            Assert.IsTrue(reservation.CanBeCancelledBy(new User { IsAdmin = true }));
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_UserIsMadeBy_ReturnsTrue()
+        {
+            var user = new User();
+            var reservation = new Reservation { MadeBy = user };
+            Assert.IsTrue(reservation.CanBeCancelledBy(user));
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_UserIsNeitherAdminNorMadeBy_ReturnsFalse()
+        {
+            var reservation = new Reservation() { MadeBy = new User() };
+            Assert.IsFalse(reservation.CanBeCancelledBy(new User()));
         }
     }
 }
