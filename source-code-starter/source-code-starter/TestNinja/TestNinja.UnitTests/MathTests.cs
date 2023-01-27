96a12cd5d77e8e48114bfd56072c3d6f08d947e2
J D <zathrath03@gmail.com>
2023-01-27 14:26:20 -0800

    Add tests for Math.Max() method

--- source-code-starter/source-code-starter/TestNinja/TestNinja.UnitTests/MathTests.cs
+++ source-code-starter/source-code-starter/TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -10,8 +10,28 @@ namespace TestNinja.UnitTests
         public void Add_WhenCalled_ReturnTheSumOfArguments()
         {
             var math = new Math();
-            var result = math.Add(1, 2);
-            Assert.That(result, Is.EqualTo(3));
+            Assert.That(math.Add(1, 2), Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Max_FirstArgumentIsGreater_ReturnsFirstArgument()
+        {
+            var math = new Math();
+            Assert.That(math.Max(2, 1), Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_SecondArgumentIsGreater_ReturnsSecondArgument()
+        {
+            var math = new Math();
+            Assert.That(math.Max(1, 2), Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_ArgumentsAreEqual_ReturnsTheSameArgument()
+        {
+            var math = new Math();
+            Assert.That(math.Max(2, 2), Is.EqualTo(2));
         }
     }
 }
