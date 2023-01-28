3778d75af2c24d9646224bc3d425ff4b6ddb8133
J D <zathrath03@gmail.com>
2023-01-27 17:09:45 -0800

    Add test for ErrorLogger.Log() to verify that arguments that are null or whitespace result in an ArgumentNullException

--- source-code-starter/source-code-starter/TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
+++ source-code-starter/source-code-starter/TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -6,15 +6,30 @@ namespace TestNinja.UnitTests
     [TestFixture]
     public class ErrorLoggerTests
     {
+        private ErrorLogger _logger;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _logger = new ErrorLogger();
+        }
+        
         [Test]
         public void Log_ErrorIsNotNull_SetTheLastErrorProperty()
         {
             const string error = "New Error";
-            var log = new ErrorLogger();
+            _logger.Log(error);
             
-            log.Log(error);
-            
-            Assert.That(log.LastError, Is.EqualTo(error));
+            Assert.That(_logger.LastError, Is.EqualTo(error));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log_ArgumentIsNullOrWhiteSpace_ThrowArgumentNullException(string error)
+        {
+            Assert.That(() => _logger.Log(error), Throws.ArgumentNullException);
         }
     }
 }
