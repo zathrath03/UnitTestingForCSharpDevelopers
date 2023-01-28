e4292a0928f2077d583fb1d9a452021c3dd6953a
J D <zathrath03@gmail.com>
2023-01-27 16:21:43 -0800

    Add test for Math.GetOddNumbers() including alternate ways of asserting the returning state

--- source-code-starter/source-code-starter/TestNinja/TestNinja.UnitTests/MathTests.cs
+++ source-code-starter/source-code-starter/TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -28,5 +28,23 @@ namespace TestNinja.UnitTests
         {
             Assert.That(_math.Max(a, b), Is.EqualTo(expectedResult));
         }
+
+        [Test]
+        public void GetOddNumbers_LimitIsGreaterThanZero_ReturnOddNumbersUpToLimit()
+        {
+            var result = _math.GetOddNumbers(5);
+            // Assert.That(result, Is.Not.Empty);
+            
+            // Assert.That(result.Count(), Is.EqualTo(3));
+            
+            // Assert.That(result, Does.Contain(1));
+            // Assert.That(result, Does.Contain(3));
+            // Assert.That(result, Does.Contain(5));
+            
+            Assert.That(result, Is.EquivalentTo(new [] {1, 3, 5}));
+            
+            // Assert.That(result, Is.Ordered);
+            // Assert.That(result, Is.Unique);
+        }
     }
 }
