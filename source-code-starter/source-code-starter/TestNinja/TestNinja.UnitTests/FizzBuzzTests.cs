107aa69c4ff9feac0593c431a9417f9a080bdec5
J D <zathrath03@gmail.com>
2023-01-28 12:06:02 -0800

    Add TestName arguments to the testCases and rename the test method. Update test cases to the simplest values

--- source-code-starter/source-code-starter/TestNinja/TestNinja.UnitTests/FizzBuzzTests.cs
+++ source-code-starter/source-code-starter/TestNinja/TestNinja.UnitTests/FizzBuzzTests.cs
@@ -7,14 +7,13 @@ namespace TestNinja.UnitTests
     public class FizzBuzzTests
     {
         [Test]
-        [TestCase(0, "FizzBuzz")]
-        [TestCase(30, "FizzBuzz")]
-        [TestCase(6, "Fizz")]
-        [TestCase(10, "Buzz")]
-        [TestCase(2, "2")]
-        public void GetOutput_WhenCalled_ReturnsCorrectString(int num, string expectedResult)
+        [TestCase(3, "Fizz", TestName = "ArgDivisibleByOnly3_ReturnFizz")]
+        [TestCase(5, "Buzz", TestName = "ArgDivisibleByOnly5_ReturnBuzz")]
+        [TestCase(15, "FizzBuzz", TestName = "ArgDivisibleBy3And5_ReturnFizzBuzz")]
+        [TestCase(1, "1", TestName = "ArgNotDivisibleBy3Or5_ReturnStringOfArg")]
+        public void GetOutput(int number, string expectedResult)
         {
-            Assert.That(FizzBuzz.GetOutput(num), Is.EqualTo(expectedResult));
+            Assert.That(FizzBuzz.GetOutput(number), Is.EqualTo(expectedResult));
         }
     }
 }
